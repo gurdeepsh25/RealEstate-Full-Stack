8b50353e90ca384c3bbccc07477053932df1aeec
gurdeepsh25 <gurdeepsh25@gmail.com>
2024-02-28 11:51:24 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,7 +17,9 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Server for Prime-Estate");
