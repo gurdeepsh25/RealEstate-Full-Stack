c223ae42c594c9ba30f95a543d71f69ab05ad833
gurdeepsh25 <gurdeepsh25@gmail.com>
2024-02-28 08:34:00 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.get('/', (req,res)=>{
-  res.send('Hello')
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on 3000");
