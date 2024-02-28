cff338769fd8b575141d9837ad1ffb4e4ff45d89
gurdeepsh25 <gurdeepsh25@gmail.com>
2024-02-28 12:00:22 +0530

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -28,3 +28,14 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running on 3000");
 });
+
+// Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message,
+  });
+});
